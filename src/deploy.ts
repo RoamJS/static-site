26352a322c31bd6f1bd7a81e650b72ea152e2968
David Vargas <dvargas92495@gmail.com>
2021-01-13 15:32:10 -0500

    Emptying objects before shutdown and deploy

--- src/deploy.ts
+++ src/deploy.ts
@@ -25,6 +25,39 @@ export const handler = async (event: { roamGraph: string }): Promise<void> =>
     const Bucket = `roamjs-${event.roamGraph}`;
     const ContentType = "text/html";
     const filesToUpload = fs.readdirSync(path.join("/tmp", "out"));
+
+    console.log("Delete existing keys that are no longer in graph");
+    const fileSet = new Set(filesToUpload);
+    const keysToDelete = new Set<string>();
+    let finished = false;
+    let ContinuationToken: string = undefined;
+    while (!finished) {
+      const {
+        Contents,
+        IsTruncated,
+        NextContinuationToken,
+      } = await s3.listObjectsV2({ Bucket, ContinuationToken }).promise();
+      Contents.map(({ Key }) => Key)
+        .filter((k) => !fileSet.has(k))
+        .forEach((k) => keysToDelete.add(k));
+      finished = !IsTruncated;
+      ContinuationToken = NextContinuationToken;
+    }
+    if (keysToDelete.size) {
+      const DeleteObjects = Array.from(keysToDelete).map((Key) => ({ Key }));
+      for (let i = 0; i < DeleteObjects.length; i += 1000) {
+        await s3
+          .deleteObjects({
+            Bucket,
+            Delete: { Objects: DeleteObjects.slice(i, i + 1000) },
+          })
+          .promise();
+      }
+      console.log("Deleted", DeleteObjects.length, "objects!");
+    } else {
+      console.log("No keys to delete, on to uploading.");
+    }
+
     for (const Key of filesToUpload) {
       const Body = fs.createReadStream(path.join("/tmp", "out", Key));
       await s3.upload({ Bucket, Key, Body, ContentType }).promise();
--- src/shutdown.ts
+++ src/shutdown.ts
@@ -8,8 +8,26 @@ const s3 = new AWS.S3({
   },
 });
 
+const emptyBucket = async (Bucket: string) => {
+  const { Contents, IsTruncated } = await s3.listObjects({ Bucket }).promise();
+  if (Contents.length > 0) {
+    await s3
+      .deleteObjects({
+        Bucket,
+        Delete: {
+          Objects: Contents.map(({ Key }) => ({ Key })),
+        },
+      })
+      .promise();
+    if (IsTruncated) {
+      await emptyBucket(Bucket);
+    }
+  }
+};
+
 export const handler = async (event: { roamGraph: string }) => {
   const Bucket = `roamjs-${event.roamGraph}`;
+  await emptyBucket(Bucket);
   await s3
     .deleteBucket({
       Bucket,
